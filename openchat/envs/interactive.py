c9be3622b6998a033614912f83f6bedeec0f9dcd
hyunwoongko <gusdnd852@naver.com>
2021-04-08 18:09:03 +0900

    add empty cache

--- openchat/envs/interactive.py
+++ openchat/envs/interactive.py
@@ -1,6 +1,8 @@
 import random
 import sys
 
+import torch
+
 from openchat.base.agents.base import SingleTurn
 from openchat.base.agents.prompt import PromptAgent
 from openchat.base.envs.base import BaseEnvironment
@@ -43,6 +45,7 @@ class InteractiveEnvironment(BaseEnvironment):
         self.clear_histories(self.user_id)
 
         while True:
+            torch.cuda.empty_cache()
             if self.is_empty(self.user_id):
                 pre_dialog_output = self.pre_dialog_for_special_tasks(agent)
 
