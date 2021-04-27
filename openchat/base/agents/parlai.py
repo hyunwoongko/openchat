f571fd8b271adf3fb3ec00ed3d0fd228a3799c9b
Harold Hannon <haroldhannon@gmail.com>
2021-04-26 22:49:44 -0500

    Update parlai.py
    added gpu element and defaulted to -1 as it is in the opt file with models.  It needs to be overridden with a kwarg of the same name and set to the correct gpu id or the model doesn't get assigned to the correct gpu and and error occurs.

--- openchat/base/agents/parlai.py
+++ openchat/base/agents/parlai.py
@@ -76,6 +76,7 @@ class ParlaiGenerationAgent(ParlaiAgent):
         top_p=None,
         no_repeat_ngram_size=4,
         length_penalty: int = 0.65,
+        gpu = -1,
     ) -> Dict[str, str]:
         assert method in ["greedy", "beam", "top_k", "nucleus"], \
             "param `method` must be one of ['greedy', 'beam'', 'top_k', 'nucleus']"
@@ -87,6 +88,7 @@ class ParlaiGenerationAgent(ParlaiAgent):
         self.model.opt["beam-block.ngram"] = no_repeat_ngram_size
         self.model.opt["beam-context-block-ngram"] = no_repeat_ngram_size
         self.model.opt["beam_length_penalty"] = length_penalty
+        self.model.opt["gpu"] = gpu
 
         message = Message({
             "text": text,
