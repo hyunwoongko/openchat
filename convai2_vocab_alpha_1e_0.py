a8cc9d9257b504a9b9e471b259f274392a848cb6
haroldhannon <haroldhannon@gmail.com>
2021-04-27 23:08:02 -0500

    Test Scripts initial pass

---
+++ convai2_vocab_alpha_1e_0.py
@@ -0,0 +1,4 @@
+from openchat import OpenChat
+
+if __name__ == '__main__':
+    OpenChat(model="unlikelihood.convai2.vocab.alpha.1e-1", device="cuda", gpu=0)
---
+++ wow_e2e.py
@@ -0,0 +1,4 @@
+from openchat import OpenChat
+
+if __name__ == '__main__':
+    OpenChat(model="wizard_of_wikipedia.end2end_generator", device="cuda:0")
